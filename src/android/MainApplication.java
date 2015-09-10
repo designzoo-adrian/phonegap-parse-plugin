b4a799694be47bd449dc7c3bb0d5f38fdf07a34f
Adrian Campbell <adrian@thedesignzoo.co.uk>
2015-09-10 15:19:02 +0100

    Add Main Application File

---
+++ src/android/MainApplication.java
@@ -0,0 +1,13 @@
+package org.apache.cordova.core;
+
+import org.apache.cordova.core.ParsePlugin;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ParsePlugin.initializeParseWithApplication(this);
+    }
+
+}
